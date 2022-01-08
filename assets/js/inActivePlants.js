a3faccc2732ae691f2ebb2aed27e71f0339982ab
Sharif Noor <sharifnoor742743284@gmail.com>
2022-01-08 13:01:13 +0500

    inActive Plants Changes

--- assets/js/inActivePlants.js
+++ assets/js/inActivePlants.js
@@ -11,6 +11,7 @@
       // Get a reference to the database service
       var database = firebase.database();
       var deviceName;
+      var reload = false;
       var div;
       var singleDeviceData;
       var singleDeviceFlowRate;
@@ -19,36 +20,52 @@
       const dbRefObject = firebase.database().ref();
       dbRefObject.on('value', snap => {
         var data = snap.val();
-        var length = Object.keys(data).length;
-        for (let i = 0; i < length; i++) {
-          deviceName = Object.keys(data)[i];
-          singleDeviceData = data[deviceName];
-          singleDeviceLastUpdate = singleDeviceData.LastUpdate;
-          var currentDateTime  = new Date();
-          var currentDateTimeInSec = currentDateTime.setSeconds(currentDateTime.getSeconds()-3);
-          var singleDeviceLastUpdateInSec = Date.parse(singleDeviceLastUpdate) - 18000000; // Converting UTC GMT time to PST local time by Subtracting 18,000,000 as PST = GMT +5
-          if(singleDeviceLastUpdateInSec < currentDateTimeInSec - 90){
-            if (!singleDeviceData.UnderMintanance){
-              if (document.getElementById(deviceName)){
-                singleDeviceData = data[deviceName];
-                singleDeviceFlowRate = singleDeviceData.FlowRateFromFloatSwitch;
-                singleDeviceTotalFlow = singleDeviceData.TotalLitresFromFloatSwitch;
-                singleDeviceLastUpdate = singleDeviceData.LastUpdate.split("T", 2);
-                document.getElementById(deviceName).innerHTML = "<div style='border-radius: 25px' class='p-2 bg-danger bg-gradient text-white'><h3 class='text-center'>" + deviceName + "</h3>" + '<p>Flow Rate: ' + singleDeviceFlowRate + '</p><p>Total Flow: ' + singleDeviceTotalFlow + '</p><p>Last Update: ' + singleDeviceLastUpdate[0] + " " + singleDeviceLastUpdate[1].slice(0, -1) + '</p></div>';
-              } 
+        if (data === null) {
+          reload = true;
+        }
+        else{
+          var length = Object.keys(data).length;
+          for (let i = 0; i < length; i++) {
+            deviceName = Object.keys(data)[i];
+            singleDeviceData = data[deviceName];
+            singleDeviceLastUpdate = singleDeviceData.LastUpdate;
+            var currentDateTime  = new Date();
+            var currentDateTimeInSec = currentDateTime.setSeconds(currentDateTime.getSeconds()-3);
+            var singleDeviceLastUpdateInSec = Date.parse(singleDeviceLastUpdate) - 18000000; // Converting UTC GMT time to PST local time by Subtracting 18,000,000 as PST = GMT +5
+            var t =currentDateTimeInSec - 90 * 6000;
+            if(singleDeviceLastUpdateInSec < t){
+              if (!singleDeviceData.UnderMintanance){
+                if (document.getElementById(deviceName)){
+                  singleDeviceData = data[deviceName];
+                  singleDeviceFlowRate = singleDeviceData.FlowRateFromFloatSwitch;
+                  singleDeviceTotalFlow = singleDeviceData.TotalLitresFromFloatSwitch;
+                  singleDeviceLastUpdate = singleDeviceData.LastUpdate.split("T", 2);
+                  document.getElementById(deviceName).innerHTML = "<div style='border-radius: 25px' class='p-2 bg-danger bg-gradient text-white'><h3 class='text-center'>" + deviceName + "</h3>" + '<p>Flow Rate: ' + singleDeviceFlowRate + '</p><p>Total Flow: ' + singleDeviceTotalFlow + '</p><p>Last Update: ' + singleDeviceLastUpdate[0] + " " + singleDeviceLastUpdate[1].slice(0, -1) + '</p></div>';
+                } 
+                else {
+                  div = document.createElement('div');
+                  div.id = deviceName;
+                  div.className = 'col p-1';
+                  outer.appendChild(div);
+                  singleDeviceData = data[deviceName];
+                  singleDeviceFlowRate = singleDeviceData.FlowRateFromFloatSwitch;
+                  singleDeviceTotalFlow = singleDeviceData.TotalLitresFromFloatSwitch;
+                  singleDeviceLastUpdate = singleDeviceData.LastUpdate.split("T", 2);
+                  div.innerHTML = "<div style='border-radius: 25px' class='p-2 bg-danger bg-gradient text-white'><h3 class='text-center'>" + deviceName + "</h3>" + '<p>Flow Rate: ' + singleDeviceFlowRate + '</p><p>Total Flow: ' + singleDeviceTotalFlow + '</p><p>Last Update: ' + singleDeviceLastUpdate[0] + " " + singleDeviceLastUpdate[1].slice(0, -1) + '</p></div>';
+                }
+              }
               else {
-                div = document.createElement('div');
-                div.id = deviceName;
-                div.className = 'col p-1';
-                outer.appendChild(div);
-                singleDeviceData = data[deviceName];
-                singleDeviceFlowRate = singleDeviceData.FlowRateFromFloatSwitch;
-                singleDeviceTotalFlow = singleDeviceData.TotalLitresFromFloatSwitch;
-                singleDeviceLastUpdate = singleDeviceData.LastUpdate.split("T", 2);
-                div.innerHTML = "<div style='border-radius: 25px' class='p-2 bg-danger bg-gradient text-white'><h3 class='text-center'>" + deviceName + "</h3>" + '<p>Flow Rate: ' + singleDeviceFlowRate + '</p><p>Total Flow: ' + singleDeviceTotalFlow + '</p><p>Last Update: ' + singleDeviceLastUpdate[0] + " " + singleDeviceLastUpdate[1].slice(0, -1) + '</p></div>';
+                if (document.getElementById(deviceName)){
+                  // document.getElementById(deviceName).remove();
+                }
+              }
+            } 
+            else {
+              if (document.getElementById(deviceName)){
+                // document.getElementById(deviceName).remove();
               }
             }
-          } 
+          }
         }
       });
     }());
