7195a147f69e254740e9d71e843cfaf44d2a5625
Sharif Noor <sharifnoor742743284@gmail.com>
2022-01-12 16:50:38 +0500

    Misc Changes

--- assets/js/inActivePlants.js
+++ assets/js/inActivePlants.js
@@ -1,16 +1,19 @@
+var Firestoredata = [];
 (function() {
     var config = {
         apiKey: "AIzaSyCaNA5SLdRQHM-KnBKTtHf8km6go9VvlcY",
         authDomain: "firsthundreddevices.firebaseapp.com",
         databaseURL: "https://firsthundreddevices-default-rtdb.firebaseio.com",
-        storageBucket: "firsthundreddevices.appspot.com"
+        storageBucket: "firsthundreddevices.appspot.com",
+        projectId: "firsthundreddevices"
       };
     
       firebase.initializeApp(config);
+      dataFromFirestore();
     
       // Get a reference to the database service
       var database = firebase.database();
-      var deviceName;
+      var deviceNamee;
       var reload = false;
       var div;
       var singleDeviceData;
@@ -26,45 +29,59 @@
         else{
           var length = Object.keys(data).length;
           for (let i = 0; i < length; i++) {
-            deviceName = Object.keys(data)[i];
-            singleDeviceData = data[deviceName];
+            deviceNamee = Object.keys(data)[i];
+            singleDeviceData = data[deviceNamee];
             singleDeviceLastUpdate = singleDeviceData.LastUpdate;
             var currentDateTime  = new Date();
             var currentDateTimeInSec = currentDateTime.setSeconds(currentDateTime.getSeconds()-3);
             var singleDeviceLastUpdateInSec = Date.parse(singleDeviceLastUpdate) - 18000000; // Converting UTC GMT time to PST local time by Subtracting 18,000,000 as PST = GMT +5
             if(singleDeviceLastUpdateInSec < currentDateTimeInSec - 90000){
               if (!singleDeviceData.UnderMintanance){
-                if (document.getElementById(deviceName)){
-                  singleDeviceData = data[deviceName];
-                  singleDeviceFlowRate = singleDeviceData.FlowRateFromFloatSwitch;
-                  singleDeviceTotalFlow = singleDeviceData.TotalLitresFromFloatSwitch;
-                  singleDeviceLastUpdate = singleDeviceData.LastUpdate.split("T", 2);
-                  document.getElementById(deviceName).innerHTML = "<div style='border-radius: 25px' class='p-2 bg-danger bg-gradient text-white'><h3 class='text-center'>" + deviceName + "</h3>" + '<p>Flow Rate: ' + singleDeviceFlowRate + '</p><p>Total Flow: ' + singleDeviceTotalFlow + '</p><p>Last Update: ' + singleDeviceLastUpdate[0] + " " + singleDeviceLastUpdate[1].slice(0, -1) + '</p></div>';
-                } 
+                if (document.getElementById(deviceNamee)){
+                  for (var n = 0; n < Firestoredata.length; n++){
+                    if (Firestoredata[n].DeviceID === deviceNamee){
+                      document.getElementById(deviceNamee).innerHTML = "<div style='border-radius: 25px' class='p-2 bg-danger bg-gradient text-white'><h3 class='text-center'>" + Firestoredata[n].DeviceName + "</h3>" + '<p>Taluqa: ' + Firestoredata[n].Taluqa + '</p><p>Location: ' + Firestoredata[n].Location + '</p></div>';
+                    }
+                  }
+                }
                 else {
-                  div = document.createElement('div');
-                  div.id = deviceName;
-                  div.className = 'col p-1';
-                  outer.appendChild(div);
-                  singleDeviceData = data[deviceName];
-                  singleDeviceFlowRate = singleDeviceData.FlowRateFromFloatSwitch;
-                  singleDeviceTotalFlow = singleDeviceData.TotalLitresFromFloatSwitch;
-                  singleDeviceLastUpdate = singleDeviceData.LastUpdate.split("T", 2);
-                  div.innerHTML = "<div style='border-radius: 25px' class='p-2 bg-danger bg-gradient text-white'><h3 class='text-center'>" + deviceName + "</h3>" + '<p>Flow Rate: ' + singleDeviceFlowRate + '</p><p>Total Flow: ' + singleDeviceTotalFlow + '</p><p>Last Update: ' + singleDeviceLastUpdate[0] + " " + singleDeviceLastUpdate[1].slice(0, -1) + '</p></div>';
+                  for (var m = 0; m < Firestoredata.length; m++){
+                    if (Firestoredata[m].DeviceID === deviceNamee){
+                      div = document.createElement('div');
+                      div.id = deviceNamee;
+                      div.className = 'col p-1';
+                      outer.appendChild(div);
+                      div.innerHTML = "<div style='border-radius: 25px' class='p-2 bg-danger bg-gradient text-white'><h3 class='text-center'>" + Firestoredata[m].DeviceName + "</h3>" + '<p>Taluqa: ' + Firestoredata[m].Taluqa + '</p><p>Location: ' + Firestoredata[m].Location + '</p></div>';
+                    }
+                  }
                 }
               }
               else {
-                if (document.getElementById(deviceName)){
-                  document.getElementById(deviceName).remove();
+                if (document.getElementById(deviceNamee)){
+                  document.getElementById(deviceNamee).remove();
                 }
               }
             } 
             else {
-              if (document.getElementById(deviceName)){
-                document.getElementById(deviceName).remove();
+              if (document.getElementById(deviceNamee)){
+                document.getElementById(deviceNamee).remove();
               }
             }
           }
         }
       });
+      
     }());
+
+    function dataFromFirestore () {
+      let fireStore = firebase.firestore();
+      fireStore.collection("Devices").get().then((deviceID)=>{
+        deviceID.forEach(singleDevice => {
+            var deviceData = singleDevice.data();
+            if(!Firestoredata.includes(deviceData)){
+              Firestoredata.push(deviceData);
+            }
+        });
+      });
+      console.log(Firestoredata);
+    }
--- assets/js/showDetail.js
+++ assets/js/showDetail.js
@@ -5,16 +5,30 @@ function showDetail () {
     var CurrentFlowRate = document.getElementById('TotalCurrentFlowRate');
     var TotalFlow = document.getElementById('TotalFlow');
     var DeviceMonitoringDiv = document.getElementById('DeviceMonitoringDiv');
+    var DeviceInactive = document.getElementById('DeviceInactive');
     var DeviceID = ddlDeviceName.value;
     var data;
+    var singleDeviceLastUpdate;
 
     if (DeviceID !== 'Select Device'){
         const dbRefObject = firebase.database().ref();
         dbRefObject.on('value', snap => {
           data = snap.val();
-          CurrentFlowRate.innerText = data[DeviceID].FlowRateFromFloatSwitch;
-          TotalFlow.innerText = data[DeviceID].TotalLitresFromFloatSwitch;
-          DeviceMonitoringDiv.style.display = 'flex';
+          singleDeviceLastUpdate = data[DeviceID].LastUpdate;
+          var currentDateTime  = new Date();
+          var currentDateTimeInSec = currentDateTime.setSeconds(currentDateTime.getSeconds()-3);
+          var singleDeviceLastUpdateInSec = Date.parse(singleDeviceLastUpdate) - 18000000; // Converting UTC GMT time to PST local time by Subtracting 18,000,000 as PST = GMT +5
+          if(singleDeviceLastUpdateInSec > currentDateTimeInSec - 90){
+            CurrentFlowRate.innerText = data[DeviceID].FlowRateFromFloatSwitch;
+            TotalFlow.innerText = data[DeviceID].TotalLitresFromFloatSwitch;
+            DeviceMonitoringDiv.style.display = 'flex';
+          }
+          else {
+            CurrentFlowRate.innerText = 0;
+            TotalFlow.innerText = data[DeviceID].TotalLitresFromFloatSwitch;
+            DeviceMonitoringDiv.style.display = 'flex';
+            DeviceInactive.style.display = 'block';
+          }
         });
         
     }
