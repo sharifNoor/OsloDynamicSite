3e2ce85828ecbdf2c41cfa52ea62ebbea13b3b8f
Sharif Noor <sharifnoor742743284@gmail.com>
2022-01-13 16:59:05 +0500

    Google Maps Done

--- assets/js/deviceDetails.js
+++ assets/js/deviceDetails.js
@@ -1,5 +1,8 @@
-var Firestoredata = [];
 (function () {
+    var Firestoredata = [];
+    
+    var Latitude, Longitude;
+    var iframe = document.getElementById('iframe');
     var config = {
         apiKey: "AIzaSyCaNA5SLdRQHM-KnBKTtHf8km6go9VvlcY",
         authDomain: "firsthundreddevices.firebaseapp.com",
@@ -8,18 +11,18 @@ var Firestoredata = [];
         projectId: "firsthundreddevices"
     };
     firebase.initializeApp(config);
-    dataFromFirestore();
-})
-
-function dataFromFirestore() {
     let fireStore = firebase.firestore();
     fireStore.collection("Devices").get().then((deviceID) => {
-      deviceID.forEach(singleDevice => {
-        var deviceData = singleDevice.data();
-        if (!Firestoredata.includes(deviceData)) {
-          Firestoredata.push(deviceData);
-        }
-      });
+        deviceID.forEach(singleDevice => {
+          var deviceData = singleDevice.data();
+          if (!Firestoredata.includes(deviceData)) {
+            Firestoredata.push(deviceData);
+          }
+          if (deviceData.Latitude !== undefined){
+             Latitude = deviceData.Latitude;
+             Longitude = deviceData.Longitude;
+             iframe.src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAlFfikVxbKgBIcrCDFwbdnJJnFzTtkM50&q=" + Latitude + "," + Longitude;
+          }
+        });
     });
-    console.log(Firestoredata);
-  }
+}());
