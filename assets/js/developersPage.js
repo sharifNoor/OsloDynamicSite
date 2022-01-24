9f28e4ad3ef37290a5111fe9861d8dc93e7bf385
Sharif Noor <sharifnoor742743284@gmail.com>
2022-01-24 17:06:24 +0500

    Responsive Working

--- assets/js/developersPage.js
+++ assets/js/developersPage.js
@@ -41,7 +41,7 @@ const CheckPermissions = async () => {
         var body = document.getElementById('underMaintananceDevicesDiv');
         var tbl = document.getElementById('underMaintananceDevicesTable');
         tbl.style.width = '100%';
-        tbl.className = 'table table-hover';
+        tbl.className = 'table table-hover table-responsive';
         var tbdy = document.createElement('tbody');
         for (var i = 0; i < length; i++) {
             deviceName = Object.keys(data)[i];
